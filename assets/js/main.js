96618ff27bd1fc5907b5866a0c0080656799ed82
Federico Moretti <hello@federicomoretti.it>
2019-03-25 15:21:21 +0100

    update assets/css/main.css, assets/js/main.js, and index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+  /**
+   * Progress
+   */
+  $(window).on("scroll", function() {
+    var scrollTop = $(window).scrollTop(),
+        documentHeight = $(document).height(),
+        windowHeight = $(window).height();
+        scrollPercentage = (scrollTop / (documentHeight - windowHeight)) * 100;
+    var progressValue = scrollPercentage;
+    $("progress").attr("value", progressValue);
+  });
+
+  /**
+   * Swiper v4.5.0
+   */
   var swiper = new Swiper (".swiper-container", {
     centeredSlides: true,
     speed: 500,
